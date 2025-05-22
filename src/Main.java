543bccbbe2a9299be3ace4924002fe5d0319334a
GeekShailender <geekshailender@Shailenders-MacBook-Pro.local>
2025-05-21 22:46:59 -0400

    Inital Upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int value = 1;
+//        if (value == 1) {
+//            System.out.println("Hello");
+//        } else if (value == 2) {
+//            System.out.println("World");
+//        } else {
+//            System.out.println("Java");
+//        }
+
+    }
+}
